75bb52aa0cf75d83cb3542e5f12880240dc1ddb2
bellesbae <tsowehh@gmail.com>
2023-05-29 23:07:32 +0300

    black fixes

--- homework07/db_fill.py
+++ homework07/db_fill.py
@@ -1,14 +1,14 @@
 import random
 import sqlite3
 
-connection = sqlite3.connect('news.db')
+connection = sqlite3.connect("news.db")
 cursor = connection.cursor()
 
 cursor.execute("SELECT COUNT(*) FROM news")
 row_count = cursor.fetchone()[0]
 
 for i in range(row_count):
-    random_value = random.choices(['good', 'maybe', 'never'], [0.3, 0.5, 0.2], k=1)[0]
+    random_value = random.choices(["good", "maybe", "never"], [0.3, 0.5, 0.2], k=1)[0]
     cursor.execute("UPDATE news SET label = ? WHERE id = ?", (random_value, i))
     connection.commit()
 
