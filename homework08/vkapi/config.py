42738942a1ce37351c56933e6f295c6ee092c2d4
bellesbae <tsowehh@gmail.com>
2023-06-04 00:51:00 +0300

    req upd

--- homework08/vkapi/config.py
+++ homework08/vkapi/config.py
@@ -5,4 +5,3 @@ VK_CONFIG = {
     "access_token": "vk1.a.eR2a1otBfN-x9yAoDVU8puaml6bL21pkduO6qDEa5sOx4D_15Fte71IZy9EbHiaVdtXojKOj0rWTUER6FefpQZbai5y0FhGG9g43wzT9feRUdvZ1KAQVRv5kdH0ZqXyWNRO5mk2pLo1ZrbHe82R9MFfEFpnlNnf5XizUG805GlHnBUoHWW5yWW1GCS9Re4RH",
     "version": "5.126",
 }
-# k1.a.EGhjUwHg9qoGw2mZoxqtXVYQDiK2rTn7pprZrgvUSkq-kw12jZSlz-3lu_Hjnrpg90lJs7OETmq-z8KvKIHOJqT8GX9rJA5peWp3wo4COzXzdfQQsYNUvgUDKmDtMtiPkqoGYfPOvCmSvCU9MENlk_eHhrU8oHmut6UNrYu7BjhTSTyxR8rRbjbFMhGe9VFn&expires_in=86400&user_id=476830585
