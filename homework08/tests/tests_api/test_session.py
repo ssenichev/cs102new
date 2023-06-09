55f23d183a404865b30ee9677a05def964dfd907
bellesbae <tsowehh@gmail.com>
2023-06-09 16:20:31 +0300

    isort fixes

--- homework08/tests/tests_api/test_session.py
+++ homework08/tests/tests_api/test_session.py
@@ -3,7 +3,12 @@ import unittest
 
 import httpretty  # type: ignore
 import responses  # type: ignore
-from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError  # type: ignore
+from requests.exceptions import (  # type: ignore
+    ConnectionError,
+    HTTPError,
+    ReadTimeout,
+    RetryError
+)
 
 from homework08.vkapi.session import Session
 
